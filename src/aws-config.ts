c774760924f7dbedde0876d7e7879e8ba8dee874
yungbali <info.prodbybali@gmail.com>
2024-12-15 08:00:14 -0800

    amplify config

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,12 +1,28 @@
 import { Amplify } from 'aws-amplify';
-import config from '../amplify_outputs.json';
+
+// Try to load config from different sources
+let authConfig;
+try {
+  // First try to load from amplify_outputs.json
+  const config = require('../amplify_outputs.json');
+  authConfig = {
+    userPoolId: config.auth.user_pool_id,
+    userPoolClientId: config.auth.user_pool_client_id,
+    identityPoolId: config.auth.identity_pool_id,
+  };
+} catch (e) {
+  // Fallback to environment variables
+  authConfig = {
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+    identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
+  };
+}
 
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: config.auth.user_pool_id,
-      userPoolClientId: config.auth.user_pool_client_id,
-      identityPoolId: config.auth.identity_pool_id,
+      ...authConfig,
       signUpVerificationMethod: 'code',
       loginWith: {
         email: true
