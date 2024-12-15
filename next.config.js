0d16d37813c513022754687ed4b43999b771b28b
yungbali <info.prodbybali@gmail.com>
2024-12-15 05:34:14 -0800

    commit to amplify

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Add resolve.alias configuration
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@mui/utils/composeClasses': '@mui/utils/composeClasses/index.js',
-    };
+  webpack: (config) => {
     return config;
   },
-  transpilePackages: [
-    '@copilotkit/react-textarea',
-    '@copilotkit/react-ui',
-    '@mui/material',
-    '@mui/utils'
-  ],
+  transpilePackages: [],
 };
 
 module.exports = nextConfig; 
