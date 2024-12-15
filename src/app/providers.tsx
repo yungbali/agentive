aa76fbe616f8c55076ba118fbc9883ecc93930a3
yungbali <info.prodbybali@gmail.com>
2024-12-15 08:40:05 -0800

    Let's grt it

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,16 +2,18 @@
 
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import config from '../../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: config.auth.user_pool_id,
-      userPoolClientId: config.auth.user_pool_client_id,
-      identityPoolId: config.auth.identity_pool_id,
+      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
+      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
+      identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID!,
       signUpVerificationMethod: 'code',
+      loginWith: {
+        email: true
+      }
     }
   }
 });
