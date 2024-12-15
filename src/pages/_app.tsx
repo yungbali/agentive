35c9315ae1b715e3dc85e12e9dd3fd9fc7767a75
yungbali <info.prodbybali@gmail.com>
2024-12-15 05:59:11 -0800

    setiing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import { CopilotKit } from '@copilotkit/react-core';
 import type { AppProps } from 'next/app';
-import { ErrorBoundary } from '../components/ErrorBoundary';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ErrorBoundary>
-      <CopilotKit publicApiKey="ck_pub_b48dfc7280fc269b6ac10ab543ffc61d">
-        <Component {...pageProps} />
-      </CopilotKit>
-    </ErrorBoundary>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
